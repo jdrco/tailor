2b22a482fa880b5011325fce4d1751ba870b0aa5
Jared Drueco <drueco03@gmail.com>
2023-07-09 19:25:05 -0600

    Pass content from editor to parent

--- src/app/editor/[profileId]/page.tsx
+++ src/app/editor/[profileId]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import * as React from 'react';
 import { doc, getDoc } from 'firebase/firestore';
 import { db } from '@/firebase/config';
 import Editor from '@/components/editor';
@@ -20,104 +21,50 @@ async function getProfile(id: string) {
 }
 
 import { Button } from '@/components/ui/button';
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from '@/components/ui/hover-card';
 import { Label } from '@/components/ui/label';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { Textarea } from '@/components/ui/textarea';
 
 export default function ProfilePage({ params }: EditorPageProps) {
+  const [targetContent, setTargetContent] = React.useState(null);
+  const [inputContent, setInputContent] = React.useState(null);
+
+  const handleTargetChange = (data: any) => {
+    setTargetContent(data);
+    console.log(targetContent);
+  };
+
+  const handleInputChange = (data: any) => {
+    setInputContent(data);
+    console.log(inputContent);
+  };
+
   return (
     <>
-      <div className="md:hidden text-center">Not supported on mobile</div>
-      <div className="hidden h-full flex-col md:flex">
-        <Tabs defaultValue="manual" className="flex-1">
-          <div className="h-full">
-            <div className="grid h-full items-stretch gap-6 md:grid-cols-[1fr_200px]">
-              <div className="hidden flex-col space-y-4 sm:flex md:order-2">
-                <div className="grid gap-2">
-                  <HoverCard openDelay={200}>
-                    <HoverCardTrigger asChild>
-                      <span className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                        Mode
-                      </span>
-                    </HoverCardTrigger>
-                    <HoverCardContent className="w-[320px] text-sm" side="left">
-                      Choose the interface that best suits your needs: a simple
-                      comparison view for manual editing or some text with
-                      instructions to edit
-                    </HoverCardContent>
-                  </HoverCard>
-                  <TabsList className="grid grid-cols-2">
-                    <TabsTrigger value="manual">
-                      <span className="sr-only">manual</span>Manual
-                    </TabsTrigger>
-                    <TabsTrigger value="auto">
-                      <span className="sr-only">auto</span>Auto
-                    </TabsTrigger>
-                  </TabsList>
-                </div>
-              </div>
-              <div className="md:order-1">
-                <TabsContent value="manual" className="mt-0 border-0 p-0">
-                  <div className="flex flex-col space-y-4">
-                    <div className="grid h-full gap-6 lg:grid-cols-2">
-                      <div className="flex flex-col space-y-4">
-                        <div className="flex flex-1 flex-col space-y-2">
-                          <Label htmlFor="input">Target</Label>
-                          <Editor className="flex-1 lg:min-h-[580px] bg-muted" />
-                        </div>
-                      </div>
-                      <div className="flex flex-col space-y-4">
-                        <div className="flex flex-1 flex-col space-y-2">
-                          <Label htmlFor="input">Input</Label>
-                          <Editor className="flex-1 lg:min-h-[580px]" />
-                        </div>
-                      </div>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                      <Button>Save</Button>
-                    </div>
-                  </div>
-                </TabsContent>
-                <TabsContent value="auto" className="mt-0 border-0 p-0">
-                  <div className="flex flex-col space-y-4">
-                    <div className="grid h-full gap-6 lg:grid-cols-2">
-                      <div className="flex flex-col space-y-4">
-                        <div className="flex flex-1 flex-col space-y-2">
-                          <Label htmlFor="input">Target</Label>
-                          <Textarea
-                            id="input"
-                            placeholder="Tailor your thoughts to..."
-                            className="flex-1 lg:min-h-[580px] bg-muted"
-                          />
-                        </div>
-                      </div>
-                      <div className="flex flex-col space-y-4">
-                        <div className="flex flex-1 flex-col space-y-2">
-                          <Label htmlFor="input">Input</Label>
-                        </div>
-                        <div className="flex flex-col space-y-2">
-                          <Label htmlFor="instructions">Instructions</Label>
-                          <Textarea
-                            id="instructions"
-                            placeholder="Fix the grammar."
-                          />
-                        </div>
-                      </div>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                      <Button>Save</Button>
-                    </div>
-                  </div>
-                </TabsContent>
-              </div>
+      <div className="flex flex-col space-y-4">
+        <div className="grid h-full gap-4 grid-cols-1 lg:grid-cols-2">
+          <div className="flex flex-col">
+            <div className="flex flex-1 flex-col space-y-2">
+              <Label htmlFor="input">Target</Label>
+              <Editor
+                className="flex-1 min-h-[360px] sm:min-h-[720px] bg-muted"
+                editorType="target"
+                onChange={handleTargetChange}
+              />
             </div>
           </div>
-        </Tabs>
+          <div className="flex flex-col">
+            <div className="flex flex-1 flex-col space-y-2">
+              <Label htmlFor="input">Input</Label>
+              <Editor
+                className="flex-1 min-h-[360px] sm:min-h-[720px] h-full"
+                editorType="input"
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <Button>Save</Button>
+        </div>
       </div>
     </>
   );
--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -6,22 +6,27 @@ import StarterKit from '@tiptap/starter-kit';
 
 type EditorProps = {
   className?: string;
+  editorType?: 'input' | 'target';
+  onChange: (data: any) => void;
 };
 
-const Editor: React.FC<EditorProps> = ({ className, ...props }) => {
+const Editor: React.FC<EditorProps> = ({ className, editorType, ...props }) => {
   const editor = useEditor({
     extensions: [StarterKit],
-    content: '<p>Hello World!️</p>',
+    content:
+      editorType === 'input'
+        ? 'Tailor your words here ...'
+        : 'Target text to tailor to ...',
     editorProps: {
       attributes: {
         class: cn(
-          'prose px-3 py-2 border rounded-md dark:prose-invert',
+          'prose max-w-none px-3 py-2 border text-sm rounded-md dark:prose-invert border border-input bg-transparent ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
           className
         ),
       },
     },
     onUpdate: ({ editor }) => {
-      // console.log(editor.getJSON());
+      props?.onChange(editor.getJSON());
     },
   });
 
