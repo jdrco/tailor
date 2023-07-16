1f236571c57828e5212ef97c8a283ea4d00ef177
Jared Drueco <drueco03@gmail.com>
2023-07-16 09:16:46 -0600

    Fix height bug on profile editor page

--- src/app/editor/layout.tsx
+++ src/app/editor/layout.tsx
@@ -10,7 +10,7 @@ interface EditorLayoutProps {
 
 export default async function EditorLayout({ children }: EditorLayoutProps) {
   return (
-    <div className="flex min-h-screen flex-col space-y-6">
+    <div className="flex h-screen flex-col space-y-6">
       <header className="sticky top-0 z-40 border-b bg-background">
         <div className="container flex h-16 items-center justify-between py-4">
           <MainNav items={homeConfig.mainNav} />
